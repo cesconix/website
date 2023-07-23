bfef5ad1446699978d76ae8963b8219480ce2336
Francesco Pasqua <cesconix@me.com>
2023-07-23 17:43:50 +0200

    feat: add active nav link and empty pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import { NavLink } from "@/types"
 import Image from "next/image"
 import Link from "next/link"
 import React from "react"
+import { usePathname } from "next/navigation"
+import { clsx } from "clsx"
 
 type HeaderProps = {
   logoUrl: string
@@ -10,9 +12,11 @@ type HeaderProps = {
 }
 
 function Header(props: HeaderProps) {
+  const pathname = usePathname()
+
   return (
     <header className="sticky top-0 mx-auto flex h-24 w-full max-w-4xl items-center justify-between px-6 md:px-10">
-      <div>
+      <Link href={"/"}>
         <div className="rounded-2xl border-[1px] border-solid border-primary-600 p-[2px]">
           <Image
             src={props.logoUrl}
@@ -22,17 +26,21 @@ function Header(props: HeaderProps) {
             className="rounded-[14px]"
           />
         </div>
-      </div>
+      </Link>
       <div className="flex items-center space-x-4">
         <nav className="hidden w-fit md:block">
           <ul className="flex items-center tracking-normal">
             {props.navLinks?.map((navLink, index) => {
+              const isActive = `/${navLink.slug}` === pathname
               return (
                 <li key={navLink.slug}>
                   <Link
                     key={navLink.slug}
                     href={navLink.slug}
-                    className="mx-1 flex items-center p-3 text-sm text-foreground-600 no-underline hover:text-foreground-100"
+                    className={clsx(
+                      "mx-1 flex items-center p-3 text-sm text-foreground-600 no-underline hover:text-foreground-100",
+                      { "text-foreground-100": isActive }
+                    )}
                   >
                     <div className="mr-2 text-xs text-primary-600">
                       {(index + 1).toString().padStart(2, "0")}.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { HomePageQuery, useHomePageQuery } from "@/types/codegen"
 
 import { graphqlClient } from "@/utils"
 import { WhoAmI } from "@/components"
-import Image from "next/image"
 
 export const getStaticProps: GetStaticProps<HomePageQuery> = async () => {
   const props = await useHomePageQuery.fetcher(graphqlClient)()
@@ -21,21 +20,19 @@ export default function Page(
   return (
     <>
       <Head>
-        <title>Francesco Pasqua</title>
+        <title>cesco.me</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="mx-auto flex min-h-[calc(100dvh-96px)] max-w-4xl items-center px-6 md:px-10">
-        <div className="">
-          <WhoAmI
-            welcome={props.homePage?.welcome as string}
-            fullname={props.homePage?.fullname as string}
-            tagline={props.homePage?.tagline as string}
-            shortBio={props.homePage?.shortBio as string}
-            socialLinks={props.common?.socialLinks!}
-          />
-        </div>
+        <WhoAmI
+          welcome={props.homePage?.welcome as string}
+          fullname={props.homePage?.fullname as string}
+          tagline={props.homePage?.tagline as string}
+          shortBio={props.homePage?.shortBio as string}
+          socialLinks={props.common?.socialLinks!}
+        />
         {/*<div className="w-full columns-2 gap-4 md:columns-3">
           {props.homePage?.gallery.map((image, index) => {
             const isShort =
