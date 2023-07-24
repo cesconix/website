d551a73b388de421ba61650649e68f1334dc8d2f
Francesco Pasqua <cesconix@me.com>
2023-07-24 02:17:13 +0200

    feat: mobile navigation menu

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,8 @@ type FooterProps = {}
 
 function Footer(props: FooterProps) {
   return (
-    <footer className="bg-neutral-600 text-sm text-foreground-700">
-      <div className="container flex h-20 items-center">
-        © 2023 Francesco Pasqua. All rights reserved.
-      </div>
+    <footer className="mx-auto flex h-24 w-full max-w-4xl items-center justify-between border-t border-t-neutral-500 px-6 text-sm text-foreground-700 md:px-10">
+      © 2023 Francesco Pasqua. All rights reserved.
     </footer>
   )
 }
---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+import * as Dialog from "@radix-ui/react-dialog"
+import HamburgerMenuIcon from "./HamburgerMenuIcon"
+import { NavLink } from "@/types"
+import NavLinks from "./NavLinks"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import CloseIcon from "./CloseIcon"
+
+type HamburgerMenuProps = {
+  navLinks: NavLink[]
+}
+
+function HamburgerMenu(props: HamburgerMenuProps) {
+  const router = useRouter()
+
+  const [open, setOpen] = useState(false)
+  const [container, setContainer] = useState(null)
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setOpen(false)
+    }
+
+    router.events.on("routeChangeStart", handleRouteChange)
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange)
+    }
+  }, [router])
+
+  return (
+    <div>
+      <Dialog.Root open={open} onOpenChange={setOpen}>
+        <Dialog.Trigger asChild>
+          <button className="text-foreground-100">
+            <HamburgerMenuIcon />
+          </button>
+        </Dialog.Trigger>
+        <Dialog.Portal container={container}>
+          <Dialog.Content className="fixed inset-0 z-10 mx-auto flex max-w-4xl flex-col justify-between bg-neutral-700 focus:outline-none">
+            <NavLinks data={props.navLinks} isMobile />
+            <Link
+              href={""}
+              className="mx-6 mb-6 block whitespace-nowrap bg-primary-600 px-5 py-3 text-center text-sm font-bold text-neutral-100 no-underline "
+            >
+              Download CV
+            </Link>
+            <Dialog.Close asChild>
+              <button
+                className="absolute right-[32px] top-[26px] inline-flex h-10 w-10 appearance-none items-center justify-center focus:outline-none"
+                aria-label="Close"
+              >
+                <CloseIcon />
+              </button>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+      {/* @ts-ignore */}
+      <div ref={setContainer} />
+    </div>
+  )
+}
+
+export default HamburgerMenu
