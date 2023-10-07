a0bd563c7cec2026b2b8ec21e7baca63c0c78abf
Francesco Pasqua <cesconix@me.com>
2023-10-08 00:03:01 +0200

    chore: minor changes

--- src/components/blocks/project-block/project-block.tsx
+++ src/components/blocks/project-block/project-block.tsx
@@ -1,18 +1,26 @@
+import Link from "next/link"
+import { GitHubLogoIcon } from "@radix-ui/react-icons"
 import { isParagraph } from "datocms-structured-text-utils"
 import { renderNodeRule, StructuredText } from "react-datocms/structured-text"
 
 import { ProjectBlockFragment } from "@/types/codegen/graphql"
+import { ArrowIcon } from "@/components/icons"
 
 function ProjectBlock(props: ProjectBlockFragment) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-10">
       {props.projects.map((entry) => (
-        <div key={entry.id} className="bg-neutral-600 p-5 space-y-4">
-          <div>
-            <div className="font-medium text-foreground-200 text-lg">
-              {entry.title}
+        <div
+          key={entry.id}
+          className="bg-neutral-600 p-5 space-y-4 hover:md:bg-neutral-500"
+        >
+          <div className="flex items-center gap-3">
+            <div>
+              <div className="font-medium text-foreground-200 text-lg flex items-center gap-1">
+                {entry.title}
+              </div>
+              <div className="text-base text-foreground-700">{entry.date}</div>
             </div>
-            <div className="text-base text-foreground-700">{entry.date}</div>
           </div>
           <div className="space-y-4 text-foreground-500">
             <StructuredText
@@ -29,6 +37,14 @@ function ProjectBlock(props: ProjectBlockFragment) {
               {...props}
             />
           </div>
+          <Link
+            href={entry.link}
+            target="_blank"
+            className="text-primary-600 text-base flex items-center gap-1 hover:underline underline-offset-2 "
+          >
+            View Repository
+            <ArrowIcon />
+          </Link>
         </div>
       ))}
     </div>
